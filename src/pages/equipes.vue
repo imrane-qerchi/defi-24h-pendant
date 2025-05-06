e94fff9852d30f47f25c381ab68bbe096b5c5c52
Imrane QERCHI <150057046+imrane-qerchi@users.noreply.github.com>
2025-05-06 10:21:22 +0200

    Centrage des cards équipe

--- src/pages/equipes.vue
+++ src/pages/equipes.vue
@@ -82,7 +82,7 @@ onMounted(() => {
       <!-- Cards -->
       <div
         v-if="teams.length"
-        class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:mt-20"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:mt-20 place-items-center"
       >
         <CardDefi
           v-for="(team, index) in teams"
